e4ea9aa1d6725760da2b0f3a2d067098a914aba1
Wesley <fariawesley4@gmail.com>
2020-05-18 16:50:28 -0300

    FIx #8

--- Weekly Game Jam 149/Assets/Scripts/Managers/GameManager.cs
+++ Weekly Game Jam 149/Assets/Scripts/Managers/GameManager.cs
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void ChooseRandomOrder()
+    public void ChooseRandomOrder()
     {
         int recipesCount;
         int randomRecipeNumber;
